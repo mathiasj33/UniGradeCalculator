ea4dba455c1be0010e99b9be1ae671a50c183c60
Mathias Jackermeier <mathias.jackermeier@tum.de>
2017-09-28 14:13:19 +0200

    Started the implementation of the login system

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, url_for
+from __future__ import print_function
+from flask import Flask, render_template, url_for, request, redirect
+import sys
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main():
+    print('Hello world', file=sys.stderr)
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if not username == "mathiasj" and password == 'selorenus12':
+        return redirect('/') # TODO: hier weiter machen
 
 
 def init_static_files():
--- static/index.js
+++ static/index.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-    $("#signInBtn").click(function () {
+    $("#signin-btn").click(function () {
         $(".fade-out").fadeOut(500, function() {
             $(".login-form").fadeIn(500);
+        });
+    });
+
+    $("#back-btn").click(function() {
+        $(".login-form").fadeOut(500, function() {
+            $(".fade-out").fadeIn(500);
         });
     });
 });
